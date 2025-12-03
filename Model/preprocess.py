0c650d428a6110efd711d8b1b9df9d8b86a1c382
water-plant <158985464+water-plant@users.noreply.github.com>
2025-12-02 20:36:22 -0800

    example notebook of preprocessing and model fitting

--- Model/preprocess.py
+++ Model/preprocess.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import os
 
+from preprocessing.preprocessor import DataPreprocessor
+from preprocessing.utils import load_data
+from preprocessing.utils import save_processed_data
+
+
 file_path = os.path.join('External_Data', 'TOTALSA.csv')
 df = pd.read_csv(file_path)
+
+preprocessor = DataPreprocessor()
+
+df = load_data(file_path)
+
+X, y = df.drop('TOTALSA',axis=1), df['TOTALSA']
 
